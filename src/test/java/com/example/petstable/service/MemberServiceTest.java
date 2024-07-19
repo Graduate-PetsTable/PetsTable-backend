a97e4de775757492007f82303426d37ae62b15bf
Seung-9 <ssg9505fj22@naver.com>
2024-07-19 18:56:22 +0900

    test: 회원 프로필 이미지 등록 구현 테스트 코드

--- src/test/java/com/example/petstable/service/MemberServiceTest.java
+++ src/test/java/com/example/petstable/service/MemberServiceTest.java
@@ -2,23 +2,29 @@ package com.example.petstable.service;
 
 import com.example.petstable.domain.board.entity.BoardEntity;
 import com.example.petstable.domain.board.repository.BoardRepository;
-import com.example.petstable.domain.bookmark.repository.BookmarkRepository;
 import com.example.petstable.domain.bookmark.service.BookmarkService;
 import com.example.petstable.domain.member.dto.request.OAuthMemberSignUpRequest;
 import com.example.petstable.domain.member.dto.response.BookmarkMyList;
+import com.example.petstable.domain.member.dto.response.MemberProfileImageResponse;
 import com.example.petstable.domain.member.entity.MemberEntity;
 import com.example.petstable.domain.member.entity.SocialType;
 import com.example.petstable.domain.member.repository.MemberRepository;
 import com.example.petstable.domain.member.service.MemberService;
 import com.example.petstable.global.exception.PetsTableException;
+import com.example.petstable.global.support.AwsS3Uploader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MemberServiceTest {
@@ -29,8 +35,8 @@ public class MemberServiceTest {
     @Autowired
     private MemberService memberService;
 
-    @Autowired
-    private BookmarkRepository bookmarkRepository;
+    @MockBean
+    private AwsS3Uploader awsS3Uploader;
 
     @Autowired
     private BookmarkService bookmarkService;
@@ -129,5 +135,29 @@ public class MemberServiceTest {
 
         // then
         assertThat(actual.getRecipes().size()).isEqualTo(3);
+    }
+
+    @DisplayName("사용자가 프로필 이미지를 등록한다.")
+    @Test
+    void addProfileImage() throws IOException {
+
+        // given
+        MemberEntity member = MemberEntity.builder()
+                .nickName("sg")
+                .socialType(SocialType.TEST)
+                .build();
+
+        memberRepository.save(member);
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("test_img", "test_img.jpg", "jpg", new FileInputStream("src/test/resources/images/test_img.jpg"));
+        when(awsS3Uploader.uploadImage(mockMultipartFile)).thenReturn("test_img.jpg");
+
+        // when
+        memberService.registerProfileImage(member.getId(), mockMultipartFile);
+
+        MemberEntity actual = memberRepository.findById(member.getId()).orElseThrow();
+
+        // then
+        assertThat(actual.getImage_url()).isEqualTo("test_img.jpg");
     }
 }
