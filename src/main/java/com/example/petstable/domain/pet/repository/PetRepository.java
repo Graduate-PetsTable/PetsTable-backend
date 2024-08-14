ccfa4fc58a26e15ee158eefc363087fbd3fe1f35
Seung-9 <ssg9505fj22@naver.com>
2024-08-14 19:11:41 +0900

    chore: 스케줄러 관련 commit 빠뜨린 거 추가

--- src/main/java/com/example/petstable/domain/pet/repository/PetRepository.java
+++ src/main/java/com/example/petstable/domain/pet/repository/PetRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<PetEntity, Long> {
 
     Optional<PetEntity> findByName(String name);
+
     List<PetEntity> findByMemberId(Long id);
 
     @Query("select m from PetEntity m where m.id = :id and m.member.id = :memberId")
@@ -18,4 +20,7 @@ public interface PetRepository extends JpaRepository<PetEntity, Long> {
 
     @Query("select count(m) > 0 from PetEntity  m where m.member.id = :memberId and m.name = :name and m.kind = :kind")
     boolean existsByMemberIdAndNameAndKind(@Param("memberId") Long memberId, @Param("name") String name, @Param("kind") String kind);
+
+    @Query("select p from PetEntity p where p.birth = :birthday")
+    List<PetEntity> findByBirth(@Param("birthday") LocalDate birthday);
 }
