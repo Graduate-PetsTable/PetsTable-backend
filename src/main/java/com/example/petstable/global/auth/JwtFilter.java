02255253f4302332d35c5802110f41067fa13865
Seung-9 <ssg9505fj22@naver.com>
2024-08-27 23:12:30 +0900

    feat: Interceptor 제거 후 Security 적용을 위해 JwtFilter 구현

---
+++ src/main/java/com/example/petstable/global/auth/JwtFilter.java
@@ -0,0 +1,56 @@
+package com.example.petstable.global.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * Spring Security 적용을 위해 JwtFilter 구현
+ */
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+    private final JwtTokenProvider jwtTokenProvider;
+
+    /**
+     * Jwt 토큰 검증 필터
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 토큰 추출
+        String token = resolveToken(request);
+
+        if (StringUtils.hasText(token)) {
+            jwtTokenProvider.validateToken(token); // 토큰 검증
+
+            // 사용자 정보를 바탕으로 접근권한 인증 객체 생성
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+            //현재 request의 Security Context에 Authentication 객체 등록
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Authorization 헤더에서 Jwt 토큰 추출
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TOKEN_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/example/petstable/global/auth/JwtTokenProvider.java
+++ src/main/java/com/example/petstable/global/auth/JwtTokenProvider.java
@@ -1,17 +1,27 @@
 package com.example.petstable.global.auth;
 
+import com.example.petstable.domain.member.entity.RoleType;
+import com.example.petstable.domain.member.entity.SocialType;
+import com.example.petstable.global.auth.dto.CustomUserDetails;
 import com.example.petstable.global.exception.PetsTableException;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 
-import static com.example.petstable.domain.member.message.OAuthLoginMessage.EXPIRED_ID_TOKEN;
-import static com.example.petstable.domain.member.message.OAuthLoginMessage.INVALID_ID_TOKEN;
+import static com.example.petstable.domain.member.message.DefaultMessage.*;
 
 @Component
 public class JwtTokenProvider {
+    private static final String AUTHORITIES_KEY = "auth";
     private final String secretKey;
     private final long validityAccessTokenInMilliseconds;
     private final JwtParser jwtParser;
@@ -23,14 +33,23 @@ public class JwtTokenProvider {
         this.jwtParser = Jwts.parser().setSigningKey(secretKey);
     }
 
-    public String createAccessToken(Long memberId) {
+    public String createAccessToken(Long memberId, String email, String provider, RoleType roleType) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityAccessTokenInMilliseconds);
 
-        return Jwts.builder()
+        Claims claims = Jwts.claims()
                 .setSubject(String.valueOf(memberId))
                 .setIssuedAt(now)
-                .setExpiration(validity)
+                .setExpiration(validity);
+
+        // claims 에 들어갈 추가 정보
+        claims.put("memberId", memberId);
+        claims.put("provider", provider);
+        claims.put("email", email);
+        claims.put(AUTHORITIES_KEY, roleType);
+
+        return Jwts.builder()
+                .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
@@ -62,5 +81,23 @@ public class JwtTokenProvider {
         } catch (JwtException e) {
             throw new PetsTableException(INVALID_ID_TOKEN.getStatus(), INVALID_ID_TOKEN.getMessage(), 401);
         }
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰으로 사용자 정보 불러오기
+        Claims claims = jwtParser.parseClaimsJws(accessToken).getBody();
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new PetsTableException(MISSING_AUTHORITY_IN_TOKEN.getStatus(), MISSING_AUTHORITY_IN_TOKEN.getMessage(), 401);
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+        UserDetails principal = new CustomUserDetails(claims.get("email").toString(), claims.get("provider", SocialType.class), authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 }
