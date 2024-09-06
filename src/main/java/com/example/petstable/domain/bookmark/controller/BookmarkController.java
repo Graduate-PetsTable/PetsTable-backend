1865fa2cd803da644636f557a2e1e9165ec5c0a4
Seung-9 <ssg9505fj22@naver.com>
2024-09-06 22:55:40 +0900

    refactor: 북마크 관련 API 부분 Swagger 문서화 작업

--- src/main/java/com/example/petstable/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/com/example/petstable/domain/bookmark/controller/BookmarkController.java
@@ -4,9 +4,6 @@ import com.example.petstable.domain.bookmark.dto.response.BookmarkRegisterRespon
 import com.example.petstable.domain.bookmark.service.BookmarkService;
 import com.example.petstable.global.auth.LoginUserId;
 import com.example.petstable.global.exception.PetsTableApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.example.petstable.domain.bookmark.message.BookmarkMessage.*;
 
-@Tag(name = "북마크 관련 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("")
-public class BookmarkController {
+public class BookmarkController implements BookmarkApi{
 
     private final BookmarkService bookmarkService;
 
-    @Operation(summary = "북마크 등록")
     @PostMapping("/{boardId}/bookmark")
-    @SecurityRequirement(name = "JWT")
     public PetsTableApiResponse<BookmarkRegisterResponse> addBookmark(@LoginUserId Long memberId, @PathVariable("boardId") Long boardId) {
 
         BookmarkRegisterResponse response = bookmarkService.registerBookmark(memberId, boardId);
