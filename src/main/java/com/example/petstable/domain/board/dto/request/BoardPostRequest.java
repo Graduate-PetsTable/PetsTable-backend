571a510e923b1bc603e8866c43a2392d5dec64a5
Seung-9 <ssg9505fj22@naver.com>
2024-09-24 04:07:22 +0900

    fix: Json 변환 오류 해결 ( @Schema -> @ArraySchema )

--- src/main/java/com/example/petstable/domain/board/dto/request/BoardPostRequest.java
+++ src/main/java/com/example/petstable/domain/board/dto/request/BoardPostRequest.java
@@ -1,5 +1,6 @@
 package com.example.petstable.domain.board.dto.request;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
@@ -13,12 +14,12 @@ public class BoardPostRequest {
     @Schema(description = "제목", example = "말티즈를 위한 닭죽 만들기")
     private String title;
 
-    @Schema(description = "설명 리스트", implementation = DescriptionRequest.class)
+    @ArraySchema(schema = @Schema(implementation = DescriptionRequest.class, description = "설명 리스d"))
     private List<DescriptionRequest> descriptions;
 
-    @Schema(description = "태그 리스트", implementation = TagRequest.class)
+    @ArraySchema(schema = @Schema(implementation = TagRequest.class, description = "태그 리스트"))
     private List<TagRequest> tags;
 
-    @Schema(description = "재료 리스트", implementation = IngredientRequest.class)
+    @ArraySchema(schema = @Schema(implementation = IngredientRequest.class, description = "재료 리스트"))
     private List<IngredientRequest> ingredients;
 }
