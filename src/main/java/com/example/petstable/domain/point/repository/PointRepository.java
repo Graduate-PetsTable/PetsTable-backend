c29d57138fc3fd13d00e6d9ef59a5dae887690d9
Seung-9 <ssg9505fj22@naver.com>
2024-10-14 04:30:13 +0900

    feat: 포인트 조회 API 구현

---
+++ src/main/java/com/example/petstable/domain/point/repository/PointRepository.java
@@ -0,0 +1,14 @@
+package com.example.petstable.domain.point.repository;
+
+import com.example.petstable.domain.point.entity.PointEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PointRepository extends JpaRepository<PointEntity, Long> {
+
+    @Query("select p from PointEntity p where p.member.id = :memberId")
+    Optional<PointEntity> findByMemberId(@Param("memberId") Long memberId);
+}
