f1bc90a113d2c72563948fda9fe95105c231565c
Seung-9 <ssg9505fj22@naver.com>
2024-10-14 04:29:59 +0900

    feat: Point 관련 ResponseMessage 구현

---
+++ src/main/java/com/example/petstable/domain/point/message/PointMessage.java
@@ -0,0 +1,17 @@
+package com.example.petstable.domain.point.message;
+
+import com.example.petstable.global.exception.ResponseMessage;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PointMessage implements ResponseMessage {
+
+    SUCCESS_GET_POINT("포인트 조회에 성공하였습니다..", HttpStatus.OK),
+    POINT_NOT_FOUND("해당 회원의 포인트 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus status;
+}
