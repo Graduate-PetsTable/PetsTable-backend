074bc067365505e0c5918461e454cdffe62616e3
Seung-9 <ssg9505fj22@naver.com>
2024-10-14 04:29:50 +0900

    feat: Point 관련 dto 구현

---
+++ src/main/java/com/example/petstable/domain/point/dto/response/PointResponse.java
@@ -0,0 +1,10 @@
+package com.example.petstable.domain.point.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PointResponse {
+    private int point;
+}
