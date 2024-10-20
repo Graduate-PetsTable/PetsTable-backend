9cff00ee106167f6b71b9463b2407a2649897d8f
Seung-9 <ssg9505fj22@naver.com>
2024-10-20 22:34:10 +0900

    feat: 레시피 작성 dto에 썸네일 이미지 추가

--- src/main/java/com/example/petstable/domain/board/dto/request/BoardWithDetailsAndTagsAndIngredients.java
+++ src/main/java/com/example/petstable/domain/board/dto/request/BoardWithDetailsAndTagsAndIngredients.java
@@ -1,6 +1,7 @@
 package com.example.petstable.domain.board.dto.request;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public class BoardWithDetailsAndTagsAndIngredients {
 
     private String title;
+    private MultipartFile thumbnail;
     private List<DetailRequest> details;
     private List<TagRequest> tags;
     private List<IngredientRequest> ingredients;
