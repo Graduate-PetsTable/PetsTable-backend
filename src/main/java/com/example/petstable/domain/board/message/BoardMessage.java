b9b71578d96ae94a9e78b4101d004a8e220519d6
Seung-9 <ssg9505fj22@naver.com>
2024-10-23 21:07:49 +0900

    feat: responseMessage 추가

--- src/main/java/com/example/petstable/domain/board/message/BoardMessage.java
+++ src/main/java/com/example/petstable/domain/board/message/BoardMessage.java
@@ -13,6 +13,8 @@ public enum BoardMessage implements ResponseMessage {
     POST_NOT_INFO("상세 내용이 없습니다.", HttpStatus.NOT_FOUND),
     GET_POST_ALL_SUCCESS("레시피 목록 조회 성공", HttpStatus.OK),
     GET_POST_DETAIL_SUCCESS("레시피 상세 조회 성공", HttpStatus.OK),
+    GET_MY_RECIPE_COUNT("나의 레시피 조회 성공", HttpStatus.OK),
+    MY_RECIPE_NOT_FOUND("나의 레시피가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     WRITE_SUCCESS("레시피 작성 성공", HttpStatus.OK),
     UPDATE_SUCCESS("레시피 수정 성공", HttpStatus.OK),
     DELETE_POST_SUCCESS("레시피 삭제 성공", HttpStatus.OK),
