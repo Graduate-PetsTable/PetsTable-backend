2cae9e15e59f8655c0c50289c92db88e6f4ac706
Seung-9 <ssg9505fj22@naver.com>
2024-10-25 20:00:58 +0900

    feat: 포인트 거래 유형을 나타내는 enum 추가

--- src/main/java/com/example/petstable/domain/point/entity/TransactionType.java
+++ src/main/java/com/example/petstable/domain/point/entity/TransactionType.java
@@ -1,7 +1,13 @@
 package com.example.petstable.domain.point.entity;
 
+import com.example.petstable.global.exception.PetsTableException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import static com.example.petstable.domain.member.message.MemberMessage.INVALID_SOCIAL;
 
 @Getter
 @RequiredArgsConstructor
@@ -9,4 +15,11 @@ public enum TransactionType {
     POINT_GAINED("획득"), POINT_USED("차감");
 
     private final String description;
+
+    public static TransactionType from(String value) {
+        return Arrays.stream(values())
+                .filter(it -> Objects.equals(it.description, value))
+                .findFirst()
+                .orElseThrow(() -> new PetsTableException(INVALID_SOCIAL.getStatus(), INVALID_SOCIAL.getMessage(), 400));
+    }
 }
