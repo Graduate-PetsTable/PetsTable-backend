b1ad346403a80269c44819a9f809d2cf6764fdeb
Seung-9 <ssg9505fj22@naver.com>
2024-10-25 20:04:10 +0900

    test: 포인트 관련 API 테스트 코드

--- src/test/java/com/example/petstable/service/PointServiceTest.java
+++ src/test/java/com/example/petstable/service/PointServiceTest.java
@@ -3,15 +3,20 @@ package com.example.petstable.service;
 import com.example.petstable.domain.member.entity.MemberEntity;
 import com.example.petstable.domain.member.entity.SocialType;
 import com.example.petstable.domain.member.repository.MemberRepository;
+import com.example.petstable.domain.point.dto.response.PointResponse;
 import com.example.petstable.domain.point.entity.PointEntity;
 import com.example.petstable.domain.point.repository.PointRepository;
 import com.example.petstable.domain.point.service.PointService;
+import com.example.petstable.global.exception.PetsTableException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,21 +37,88 @@ public class PointServiceTest {
                 .email("test@apple.com")
                 .nickName("test")
                 .socialType(SocialType.APPLE)
+                .points(new ArrayList<>())
                 .build();
         memberRepository.save(member);
 
-        PointEntity pointEntity = PointEntity.createPoint(member);
-        pointRepository.save(pointEntity);
-
         // when
-        int expected = 10;
+        int expected = 100;
 
-        pointService.increasePoints(member.getId());
-        int actual = pointService.getPointByMemberId(member.getId()).getPoint();
+        pointService.increasePoints(member, 100, "회원가입");
+        int actual = pointService.getPointHistoryByMemberId(member.getId()).getPoint();
         int actual2 = pointRepository.findByMemberId(member.getId()).orElseThrow().getPoint();
 
         // then
         assertThat(actual).isEqualTo(expected);
         assertThat(actual2).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("현재 포인트보다 차감될 포인트가 더 클 경우 예외를 반환하며 금액이 차감되어선 안 된다.")
+    void subtractPointTest() {
+        // given
+        MemberEntity member = MemberEntity.builder()
+                .email("test142@apple.com")
+                .nickName("test231")
+                .socialType(SocialType.APPLE)
+                .points(new ArrayList<>())
+                .build();
+        memberRepository.save(member);
+
+        pointService.increasePoints(member, 50, "테스트");
+
+        // when & then
+        String expectedMessage = "잔액이 부족하여 포인트를 사용할 수 없습니다.";
+        PetsTableException actualMessage = assertThrows(PetsTableException.class, () -> {
+            pointService.subtractPoints(member, 100, "포인트 사용");
+        });
+        assertTrue(actualMessage.getMessage().contains(expectedMessage));
+
+        PointEntity actual = pointRepository.findFirstByMemberIdOrderByCreatedTimeDesc(member.getId());
+        assertNotNull(actual);
+        assertThat(actual.getPoint()).isEqualTo(50); // 포인트가 50으로 유지되어야 함
+
+    }
+
+    @Test
+    @DisplayName("포인트 사용 시 포인트가 차감된다.")
+    void subtractPointExceptionTest() {
+        // given
+        MemberEntity member = MemberEntity.builder()
+                .email("test221@apple.com")
+                .nickName("test312")
+                .socialType(SocialType.APPLE)
+                .points(new ArrayList<>())
+                .build();
+        memberRepository.save(member);
+
+        // when
+        pointService.increasePoints(member, 100, "회원가입");
+        pointService.subtractPoints(member, 50, "테스트");
+
+        // then
+        int expected = 50;
+        int actual = pointRepository.findFirstByMemberIdOrderByCreatedTimeDesc(member.getId()).getBalance();
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("현재 잔액 조회에 성공한다.")
+    void getPointBalanceTest() {
+        // given
+        MemberEntity member = MemberEntity.builder()
+                .email("test552@apple.com")
+                .nickName("test930")
+                .socialType(SocialType.APPLE)
+                .points(new ArrayList<>())
+                .build();
+        memberRepository.save(member);
+
+        // when
+        pointService.increasePoints(member, 100, "회원가입");
+        int expected = 100;
+        int actual = pointService.getPointBalance(member.getId()).getPoint();
+        // then
+        assertThat(expected).isEqualTo(actual);
     }
 }
