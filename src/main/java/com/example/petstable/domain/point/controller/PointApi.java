1552988363e986ffdf7b1f8f89871606d2939655
Seung-9 <ssg9505fj22@naver.com>
2024-10-25 19:58:08 +0900

    docs: 포인트 관련 API 명세 예시 추가

--- src/main/java/com/example/petstable/domain/point/controller/PointApi.java
+++ src/main/java/com/example/petstable/domain/point/controller/PointApi.java
@@ -31,4 +31,23 @@ public interface PointApi {
                     @ApiResponse(responseCode = "500", description = "서버 오류가 발했습니다.", content = @Content)
             })
     PetsTableApiResponse<PointResponse> getMyPoint(@Parameter(hidden = true) @LoginUserId Long memberId);
+
+    @Operation(summary = "포인트 조회 API", description = "나의 포인트를 조회합니다",
+            responses =  {
+                    @ApiResponse(responseCode = "200", content = @Content(
+                            schema = @Schema(implementation = PointResponse.class),
+                            examples = @ExampleObject(value = """
+                            {
+                              "point": 10
+                              "transactionType": 획득
+                              "description": 레시피 작성
+                              "createTime": 2024-10-25 15:30:45
+                            }
+                            """)
+                    ),
+                            description = "포인트 조회에 성공하였습니다."),
+                    @ApiResponse(responseCode = "404", description = "포인트 정보를 찾을 수 없습니다.", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "서버 오류가 발했습니다.", content = @Content)
+            })
+    PetsTableApiResponse<PointResponse> getMyPointHistory(@Parameter(hidden = true) @LoginUserId Long memberId);
 }
