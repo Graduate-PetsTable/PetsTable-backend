5dad6e43d1b98f577bbb33016604f95f522eedbe
Seung-9 <ssg9505fj22@naver.com>
2024-10-25 20:34:56 +0900

    feat: 잔액만 조회하는 DTO 구현

---
+++ src/main/java/com/example/petstable/domain/point/dto/response/PointMyBalanceResponse.java
@@ -0,0 +1,10 @@
+package com.example.petstable.domain.point.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PointMyBalanceResponse {
+    private int point;
+}
