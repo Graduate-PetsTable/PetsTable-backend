4fd9cb473721c6ea568282ec4c29b4708e711233
Seung-9 <ssg9505fj22@naver.com>
2024-11-28 13:06:03 +0900

    chore: 직렬호, 역직렬화 어노테이션 추가 및 Json 형식 지정

--- src/main/java/com/example/petstable/domain/member/entity/BaseTimeEntity.java
+++ src/main/java/com/example/petstable/domain/member/entity/BaseTimeEntity.java
@@ -1,5 +1,10 @@
 package com.example.petstable.domain.member.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -8,7 +13,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
@@ -18,9 +22,15 @@ public class BaseTimeEntity {
 
     @CreatedDate
     @Column(name = "created_time", updatable = false)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime createdTime;
 
     @LastModifiedDate
     @Column(name = "modified_time")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime modifiedTime;
 }
